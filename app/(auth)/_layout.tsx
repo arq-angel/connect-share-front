08821fab9253d219ac79c1ac343639f45fd9d6c8
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-10-17 19:00:08 +1100

    clean slate before rework on design and implementation of axios and tanstack for api

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,26 +1,7 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Stack, useRouter} from "expo-router";
-import {UserLoggedInContext} from "../../context/UserLoggedIn";
-
 const AuthLayout = () => {
-    const {userLoggedIn, setUserLoggedIn} = useContext(UserLoggedInContext);
-    const router = useRouter();
-
-    useEffect(() => {
-        if (userLoggedIn) {
-            router.replace("/(home)/contact"); // Redirect to login if not authenticated
-        }
-    }, [userLoggedIn]);
-
     return (
-        <Stack>
-            <Stack.Screen
-                name="login"
-                options={{
-                    headerShown: false,
-                }}
-            />
-        </Stack>
+        <>
+        </>
     );
 };
 
