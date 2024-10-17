5d58ee7d2c552665cc7ee82c9a8066827607930a
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-10-17 20:37:05 +1100

    basic layout of the app done with primary colors

---
+++ app/(home)/favourites.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const Page = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-lg">Contacts Page</Text>
+        </View>
+    );
+};
+
+export default Page;
---
+++ app/(profile)/settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const Page = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text>Edit Profile</Text>
+        </View>
+    );
+};
+
+export default Page;
