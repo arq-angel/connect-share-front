04ea3e92b6d4770e915e862040f6cf17d80ecac6
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-10-30 22:00:38 +1100

    need to work on the profile page and maybe add a new page for editProfile and add an edit button at the end of profile page - need to find a good design - working on retrieving the employees for contact list

--- app/(auth)/(menu)/profile.tsx
+++ app/(auth)/(menu)/profile.tsx
@@ -36,90 +36,94 @@ const Page = () => {
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-        <View className="flex-1 bg-white">
-            <ScrollView
-                contentContainerStyle={{ flex: 1, backgroundColor: '#fff' }}
-            >
+            <View className="flex-1 bg-white">
+                <ScrollView
+                    contentContainerStyle={{flex: 1, backgroundColor: '#fff'}}
+                >
                     {error && (
                         <View className="flex-1 flex-col items-center justify-center">
                             <Text className="text-xl text-red-600">An unknown error occurred.</Text>
                             <Text className="text-xl text-red-600">Please close and open the app again!</Text>
                         </View>
                     )}
-                {!error && profileData && (
-                    <View className="flex-col items-center justify-center">
-                        <View className="py-2 relative">
-                            {profileData?.data?.image ? (
-                                <>
-                                    <Image
-                                        source={{uri: 'https://myapplib.com/' + profileData?.data?.image}}
-                                        className="rounded-full"
-                                        style={{height: 120, width: 120}}
-                                    />
-                                </>
+                    {!error && profileData && (
+                        <View className="flex-col items-center justify-center px-2">
+                            <View className="py-2 relative">
+                                {profileData?.data?.image ? (
+                                    <>
+                                        <Image
+                                            source={{uri: 'https://myapplib.com/' + profileData?.data?.image}}
+                                            className="rounded-full"
+                                            style={{height: 120, width: 120}}
+                                        />
+                                    </>
 
-                            ) : (profileData?.data ? (
-                                <View className="bg-gray-300 rounded-full justify-center items-center"
-                                      style={{width: 120, height: 120}}>
-                                    <Text className="text-4xl">
-                                        {profileData?.data?.firstName[0]}{profileData?.data?.lastName[0]}
-                                    </Text>
-                                </View>
-                            ) : (
-                                <View className="bg-gray-300 rounded-full justify-center items-center"
-                                      style={{width: 120, height: 120}}>
-                                    <Text className="text-4xl">
-                                        <FontAwesomeIcon icon={faUser} size={50}/>
-                                    </Text>
-                                </View>
-                            ))
-                            }
-                            <TouchableOpacity
-                                className="absolute bottom-3 right-3 border border-white rounded-full p-1.5"
-                                style={{backgroundColor: Colors.myApp.primary}}>
-                                <FontAwesomeIcon icon={faEdit} size={14} color="#ffffff" />
-                            </TouchableOpacity>
-                        </View>
-                        <View className="flex-row items-start justify-center">
-                            <View className="flex-col flex-1 space-y-2">
-                                <View className="flex-row justify-between space-x-2">
-                                    <View className="flex-1">
-                                        <Text className="text-lg">First Name</Text>
-                                        <View className="bg-gray-200 rounded-md">
-                                            <TextInput
-                                                value={profileData?.data?.firstName ?? ''}
-                                                className="p-3 text-gray-600"
-                                            />
+                                ) : (profileData?.data ? (
+                                    <View className="bg-gray-300 rounded-full justify-center items-center"
+                                          style={{width: 120, height: 120}}>
+                                        <Text className="text-4xl">
+                                            {profileData?.data?.firstName[0]}{profileData?.data?.lastName[0]}
+                                        </Text>
+                                    </View>
+                                ) : (
+                                    <View className="bg-gray-300 rounded-full justify-center items-center"
+                                          style={{width: 120, height: 120}}>
+                                        <Text className="text-4xl">
+                                            <FontAwesomeIcon icon={faUser} size={50}/>
+                                        </Text>
+                                    </View>
+                                ))
+                                }
+                                <TouchableOpacity
+                                    className="absolute bottom-3 right-3 border border-white rounded-full p-1.5"
+                                    style={{backgroundColor: Colors.myApp.primary}}>
+                                    <FontAwesomeIcon icon={faEdit} size={14} color="#ffffff"/>
+                                </TouchableOpacity>
+                            </View>
+                            <View className="flex-row items-start justify-center">
+                                {/*<View className="flex-1 flex-col">
+                                    <View className="flex-row justify-between items-center my-2">
+                                        <View className="flex-1">
+                                            <View className="mx-0.5">
+                                                <Text className="text-lg">First Name</Text>
+                                                <View className="bg-gray-200 rounded-md">
+                                                    <TextInput
+                                                        value={profileData?.data?.firstName ?? ''}
+                                                        className="p-3 text-gray-800"
+                                                    />
+                                                </View>
+                                            </View>
+                                        </View>
+                                        <View className="flex-1">
+                                            <View className="mx-0.5">
+                                                <Text className="text-lg">Middle Name</Text>
+                                                <View className="bg-gray-200 rounded-md">
+                                                    <TextInput
+                                                        value={profileData?.data?.middleName ?? ''}
+                                                        className="p-3 text-gray-800"
+                                                    />
+                                                </View>
+                                            </View>
+                                        </View>
+                                        <View className="flex-1">
+                                            <View className="mx-0.5">
+                                                <Text className="text-lg">Last Name</Text>
+                                                <View className="bg-gray-200 rounded-md">
+                                                    <TextInput
+                                                        value={profileData?.data?.lastName ?? ''}
+                                                        className="p-3 text-gray-800"
+                                                    />
+                                                </View>
+                                            </View>
                                         </View>
                                     </View>
-                                    <View className="flex-1">
-                                        <Text className="text-lg">Middle Name</Text>
-                                        <View className="bg-gray-200 rounded-md">
-                                            <TextInput
-                                                value={profileData?.data?.middleName ?? ''}
-                                                className="p-3 text-gray-600"
-                                            />
-                                        </View>
-                                    </View>
-                                    <View className="flex-1">
-                                        <Text className="text-lg">Last Name</Text>
-                                        <View className="bg-gray-200 rounded-md">
-                                            <TextInput
-                                                value={profileData?.data?.lastName ?? ''}
-                                                className="p-3 text-gray-600"
-                                            />
-                                        </View>
-                                    </View>
-                                </View>
-
-
-
+                                </View>*/}
+                                <Text>{JSON.stringify(profileData, null, 2)}</Text>
                             </View>
                         </View>
-                    </View>
-                )}
-            </ScrollView>
-        </View>
+                    )}
+                </ScrollView>
+            </View>
         </TouchableWithoutFeedback>
     );
 };
